daf6a791c3b7597a11e7424efdf1796f3d2347d5
t.jungke <t.jungke@visiconsult.de>
2021-01-17 16:39:33 +0100

    Updated nuget packages

--- Sources/MarkdownView.Portable/Extensions/ImageExtensions.cs
+++ Sources/MarkdownView.Portable/Extensions/ImageExtensions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Net;
-using SkiaSharp;
 using Xamarin.Forms;
 
 namespace Xam.Forms.MarkdownView.Extensions
@@ -11,42 +7,10 @@ namespace Xam.Forms.MarkdownView.Extensions
     {
         public static void RenderSvg(this Image view, string uri)
         {
-            try
-            {
-                var req = (HttpWebRequest)WebRequest.Create(uri);
-
-                var svg = new SkiaSharp.Extended.Svg.SKSvg();
-                req.BeginGetResponse((ar) => 
-                {
-                    var res = (ar.AsyncState as HttpWebRequest).EndGetResponse(ar) as HttpWebResponse;
-                    using (var stream = res.GetResponseStream())
-                    {
-                        if (stream != null)
-                        {
-                            var picture = svg.Load(stream);
-
-                            using (var image = SKImage.FromPicture(picture, picture.CullRect.Size.ToSizeI()))
-                            using (var data = image.Encode(SKEncodedImageFormat.Jpeg, 80))
-                            {
-                                var ms = new MemoryStream();
-
-                                if (data != null && !data.IsEmpty)
-                                {
-                                    data.SaveTo(ms);
-                                    ms.Seek(0, SeekOrigin.Begin);
-                                    ms.Position = 0;
-                                    view.Source = ImageSource.FromStream(() => ms);
-                                }
-                            }
-                        }
-                    }
-                }, req);
-
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Failed to render svg: {ex}");
-            }
+            throw new NotImplementedException();
+            
+            // SkiaSharp does not provide the SKSvg type anymore.
+            // Implement a new rendering strategy if needed (FFImageLoading based?).
         }
     }
 }
